0053d5bd6c15bc38955646bf26420ed373d74ce9
amikojhk <amikojhk@gmail.com>
2020-05-11 20:33:46 +0900

    준비,입력,출력 메소드 분리,두 가지 반복문(for,while)구현

--- src/javapractice/Calendar/MonthCheck.java
+++ src/javapractice/Calendar/MonthCheck.java
@@ -2,43 +2,60 @@ package javapractice.Calendar;
 
 import java.util.Scanner;
 
-public class MonthCheck
-{
-	public static void main(String[] args)
-	{
-		int monthDate[] = new int[12];
+public class MonthCheck {
+	public static void main(String[] args) {
+
+		int monthDate[] = monthSet();
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("몇 번 반복할까요?");
+		int month[] = monthInput(scanner.nextInt());
 		
-		for(int i = 1; i < 13; i++)
-		{
-			if(i < 8) 
-			{
-				if(i % 2 == 0)
-				{
-					monthDate[i-1] = 30;
-				}
-				else monthDate[i-1] = 31;
-			}
-			else if(i % 2 == 1)
-			{
-				monthDate[i-1] = 30;
-			}
-			else monthDate[i-1] = 31;
+		scanner.close();
+
+		System.out.println("계산 결과를 출력합니다.");
+		monthOutput(monthDate, month);
+	}
+	public static int[] monthSet() {
+		int monthDate[] = new int[12];
+		for (int i = 1; i < 13; i++) {
+			if (i < 8) {
+				if (i % 2 == 0) {
+					monthDate[i - 1] = 30;
+				} else
+					monthDate[i - 1] = 31;
+			} else if (i % 2 == 1) {
+				monthDate[i - 1] = 30;
+			} else
+				monthDate[i - 1] = 31;
 		}
 		monthDate[1] = 28;
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		String monthString;
-		System.out.println("월을 입력해주세요.");
-		monthString = scanner.next();
-		int month = Integer.parseInt(monthString);
+		return monthDate;
+	}
 
-		if(month < 13 && 0 < month)
-		{
-			System.out.printf("%d월은 %d일까지 입니다.\n", month, monthDate[month-1]);
+	public static int[] monthInput(int rc) {
+		int month[] = new int[rc];
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("월을 입력해주세요.");
+		for (int i = 0; i < rc; i++) {
+			System.out.printf("%d번째 입력:", (i + 1));
+			month[i] = scanner.nextInt();
 		}
-		else System.out.println("1부터 12 사이의 숫자를 입력해주세요.");
-		
 		scanner.close();
+		return month;
+	}
+
+	public static void monthOutput(int md[], int m[]) {
+		int i = 0;
+		while (i < m.length) {
+			if (m[i] < 13 && 0 < m[i]) {
+				System.out.printf("%d월은 %d일까지 입니다.\n", m[i], md[m[i] - 1]);
+			} else
+				System.out.println("1부터 12 사이의 숫자를 입력해주세요.");
+			i++;
+		}
 	}
 }
