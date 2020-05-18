565dc5b4f6babcd8ecad29eb05a18425896b46fb
amikojhk <amikojhk@gmail.com>
2020-05-18 11:42:07 +0900

    연도 입력 및 윤년여부 확인 기능 구현

--- src/javapractice/Calendar/CalendarClass.java
+++ src/javapractice/Calendar/CalendarClass.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class CalendarClass {
 	public static void main(String[] args) {
-		int monthDate[] = monthCheck();
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("월을 입력해주세요.");
+		System.out.println("연월을 입력해주세요.");
+		int year = 0;
 		int month = 0;
 		while (month != -1) {
-			System.out.print(">");
+			System.out.print("연> ");
+			year = scanner.nextInt();
+			System.out.print("월> ");
 			month = scanner.nextInt();
 			if (month == (-1)) {
 				System.out.print("프로그램을 종료합니다");
@@ -18,14 +20,14 @@ public class CalendarClass {
 			} else if (month > 12 || 1 > month) {
 				System.out.println("1부터 12 사이의 숫자를 입력해주세요.");
 			} else
-				calPrint(month, monthDate);
+				calPrint(year, month);
 		}
 		scanner.close();
 	}
-	
-	public static int[] monthCheck() {
+
+	public static int[] monthCheck(int year) {
 		int monthDate[] = new int[12];
-		for (int i = 1; i < 13; i++) {
+		for (int i = 1; i <= 12; i++) {
 			if (i < 8) {
 				if (i % 2 == 0) {
 					monthDate[i - 1] = 30;
@@ -36,24 +38,25 @@ public class CalendarClass {
 			} else
 				monthDate[i - 1] = 31;
 		}
-		monthDate[1] = 28;
+		if ((year % 400) == 0 || ((year % 4 == 0) && (year % 100 != 0))) {
+			monthDate[1] = 29;
+		} else
+			monthDate[1] = 28;
 		return monthDate;
-	}	
-	
-	public static void calPrint(int m, int md[]) {
-		System.out.printf("%d월 달력\n",m);
+	}
+
+	public static void calPrint(int year, int month) {
+		int monthDate[] = monthCheck(year);
+
+		System.out.printf("%d년 %d월 달력\n", year, month);
 		System.out.println("Sun Mon Tue Wed Thu Fri Sat");
-		System.out.println("---------------------------");
-		for (int i = 1; i <= md[m-1]; i++) {
-			if ((i % 7)-1 == 0) {
-			System.out.print("\n");
+		System.out.print("---------------------------");
+		for (int i = 1; i <= monthDate[month - 1]; i++) {
+			if ((i % 7) == 1) {
+				System.out.print("\n");
 			}
-			if ( i < 10) {
-				System.out.print(i + "   ");
-			} else {
-				System.out.print(i + "  ");
-			}
+			System.out.printf("%2d  ", i);
 		}
-		System.out.printf("\n%d월은 %d일까지 있습니다.\n", m, md[m-1]);
+		System.out.printf("\n%d년 %d월은 %d일까지 있습니다.\n", year, month, monthDate[month - 1]);
 	}
 }
